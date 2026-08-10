fc46b8e55122e018c72f8a725913a1c124f7bcb2
Marko <nocic.marko123@gmail.com>
2026-08-10 15:59:05 +0200

    Create 4.py

---
+++ 4.py
@@ -0,0 +1,71 @@
+import time
+import pathlib
+import random
+
+password = input("Enter the password to crack: ")
+start = time.time()
+
+def banana( start, end, descripted):
+    descripted = descripted.rstrip()
+    print("Password cracked. Password is: " + descripted)
+    print("Time for cracking: " + str(round(time.time() - start)))       
+
+def type_password(password):
+    if password.isalnum():
+        if password.islower():
+            characters = "abcdefghijklmnopqrstuvwxyz" + "0123456789"
+        elif password.isupper():
+            characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ" + "0123456789"
+    elif password.isalpha():
+        if password.islower():
+            characters = "abcdefghijklmnopqrstuvwxyz"
+        elif password.isupper():
+            characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+    elif password.isdigit():
+        characters = "0123456789"
+    elif password.isspace():
+        for i in range(18):
+            character = " "
+            if character == password:
+                end = time.time()
+                banana(start, end, password)
+                break
+            else:
+                character += " "
+    else:
+        characters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*()-_=+[]{}|;:',.<>?/`~ "
+    return characters
+
+def lofp(password):
+    lenght = len(password)
+    return lenght
+
+characters = type_password(password)
+lenght = lofp(password)
+
+def __init__(self, characters, password):
+    self.characters = characters
+    self.password = password
+    self.characters = characters
+
+
+def crack_password(self, characters, descripted):
+    for i in range(lenght): #using for the niz wwhich will be regenerated for password
+        
+    for i in range(len(characters)):
+        letter = random.choice(characters)
+        attempt += letter
+        if attempt == descripted:
+            end = time.time()
+            banana(start, end, descripted)
+            break
+        else:
+            for i in range(len(characters)):
+                letter = random.choice(characters)
+                attempt += letter
+                if attempt == descripted:
+                    end = time.time()
+                    banana(start, end, descripted)
+                    break
+                else:
+                    for i in range(len(characters)):
